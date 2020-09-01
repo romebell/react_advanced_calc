c5154a8f93256b6730f1f77feccc44683966d1d7
imjchiang <jchiang296@gmail.com>
2020-08-31 22:37:22 -0700

    [operator] implement all four operators

--- src/Calculator.js
+++ src/Calculator.js
@@ -3,41 +3,48 @@ import React, { useState } from 'react'
 const Calculator = props => {
     // Declare state variables
     let [result, setResult] = useState("Yeet");
+    let [operator, setOperator] = useState("");
 
     function clearDisplay()
     {
         setResult("");
+        setOperator("");
+    }
+
+    function createOperator(oper)
+    {
+        setOperator(oper);
     }
     
     return (
         <div className="container">
             <h1>React Calculator</h1>
             <div className="calc-container">
-                <p>{result}</p>
+                <p>{result}{operator}</p>
                 <div className="answer-box">TBD</div>
                 <div className="calc-row">
                     <button className="calc-button calc-button-top" onClick={clearDisplay}>AC</button>
                     <button className="calc-button calc-button-top">+/-</button>
                     <button className="calc-button calc-button-top">%</button>
-                    <button className="calc-button calc-button-op">/</button>
+                    <button className="calc-button calc-button-op" onClick={() => createOperator("/")}>/</button>
                 </div>
                 <div className="calc-row">
                     <button className="calc-button">7</button>
                     <button className="calc-button">8</button>
                     <button className="calc-button">9</button>
-                    <button className="calc-button calc-button-op">x</button>
+                    <button className="calc-button calc-button-op" onClick={() => createOperator("*")}>x</button>
                 </div>
                 <div className="calc-row">
                     <button className="calc-button">4</button>
                     <button className="calc-button">5</button>
                     <button className="calc-button">6</button>
-                    <button className="calc-button calc-button-op">-</button>
+                    <button className="calc-button calc-button-op" onClick={() => createOperator("-")}>-</button>
                 </div>
                 <div className="calc-row">
                     <button className="calc-button">1</button>
                     <button className="calc-button">2</button>
                     <button className="calc-button">3</button>
-                    <button className="calc-button calc-button-op">+</button>
+                    <button className="calc-button calc-button-op" onClick={() => createOperator("+")}>+</button>
                 </div>
                 <div className="calc-row">
                     <button className="calc-button width-2">0</button>

769f216bc034a79b6ac69a13ca7c6751dd8e4c4b
General Assembly <generalassembly@Davids-MacBook-Pro.local>
2020-08-31 19:27:19 -0700

    [displayNum] Add displayNum function to number buttons

--- src/Calculator.js
+++ src/Calculator.js
@@ -2,14 +2,18 @@ import React, { useState } from 'react'
 
 const Calculator = props => {
     // Declare state variables
+    const [value, setValue] = useState('');
 
+    function displayNum (num) {
+        setValue(value + num)
+    }
 
     return (
         <div className="container">
             <h1>React Calculator</h1>
             <div className="calc-container">
                 <p>Values: </p>
-                <div className="answer-box">TBD</div>
+                <div className="answer-box">{value}</div>
                 <div className="calc-row">
                     <button className="calc-button calc-button-top">AC</button>
                     <button className="calc-button calc-button-top">+/-</button>
@@ -17,25 +21,55 @@ const Calculator = props => {
                     <button className="calc-button calc-button-op">/</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">7</button>
-                    <button className="calc-button">8</button>
-                    <button className="calc-button">9</button>
+                    <button
+                        className="calc-button"
+                        onClick={() => {displayNum(7)}}
+                    >7</button>
+                    <button
+                        className="calc-button"
+                        onClick={() => {displayNum(8)}}
+                    >8</button>
+                    <button
+                        className="calc-button"
+                        onClick={() => {displayNum(9)}}
+                    >9</button>
                     <button className="calc-button calc-button-op">x</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">4</button>
-                    <button className="calc-button">5</button>
-                    <button className="calc-button">6</button>
+                    <button
+                        className="calc-button"
+                        onClick={() => {displayNum(4)}}
+                    >4</button>
+                    <button
+                        className="calc-button"
+                        onClick={() => {displayNum(5)}}
+                    >5</button>
+                    <button
+                        className="calc-button"
+                        onClick={() => {displayNum(6)}}
+                    >6</button>
                     <button className="calc-button calc-button-op">-</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button">1</button>
-                    <button className="calc-button">2</button>
-                    <button className="calc-button">3</button>
+                    <button
+                        className="calc-button"
+                        onClick={() => {displayNum(1)}}
+                    >1</button>
+                    <button
+                        className="calc-button"
+                        onClick={() => {displayNum(2)}}
+                    >2</button>
+                    <button
+                        className="calc-button"
+                        onClick={() => {displayNum(3)}}
+                    >3</button>
                     <button className="calc-button calc-button-op">+</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button width-2">0</button>
+                    <button
+                        className="calc-button width-2"
+                        onClick={() => {displayNum(0)}}
+                    >0</button>
                     <button className="calc-button">.</button>
                     <button className="calc-button calc-button-op">=</button>
                 </div>

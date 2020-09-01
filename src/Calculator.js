cb1d221754240137905b8601cb4f1c12731d5b5a
margare-jihua <margaret.jihua@gmail.com>
2020-08-31 20:56:34 -0700

    [AC] clear button

--- src/Calculator.js
+++ src/Calculator.js
@@ -3,15 +3,18 @@ import Input from './Input'
 
 const Calculator = props => {
     // Declare state variables
-    const [input, setInput] = useState(0)
-    const [operator, setOperator] = useState('')
-    const numbers = ['0','1','2','3','4','5','6','7','8','9']
+    var [input, setInput] = useState('')
+    var [firstNum, setFirstNum] = useState('')
+    var [secondNum, setSecondNum] = useState('')
+    var [operator, setOperator] = useState('')
+    var numbers = ['0','1','2','3','4','5','6','7','8','9']
 
     // Handle Click
-    const handleClick = (e) => {
-        console.log(e);
+    // handleClick() doesn't work
+     function handleClick(e) {
+        console.log(e.target.value);
         // if (numbers.includes(e)) {
-        //     setInput = e
+            // setInput (input + val)
         // }
     }
 
@@ -21,31 +24,31 @@ const Calculator = props => {
             <div className="calc-container">
                 <Input input={input} />
                 <div className="calc-row">
-                    <button className="calc-button calc-button-top">AC</button>
+                    <button className="calc-button calc-button-top" onClick={() => setInput(0)}>AC</button>
                     <button className="calc-button calc-button-top">+/-</button>
                     <button className="calc-button calc-button-top">%</button>
                     <button className="calc-button calc-button-op">/</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button" onClick={handleClick}>7</button>
-                    <button className="calc-button" onClick={handleClick}>8</button>
-                    <button className="calc-button" onClick={handleClick}>9</button>
+                    <button className="calc-button" onClick={() => setInput(input+7)}>7</button>
+                    <button className="calc-button" onClick={() => setInput(input+8)}>8</button>
+                    <button className="calc-button" onClick={() => setInput(input+9)}>9</button>
                     <button className="calc-button calc-button-op">x</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button" onClick={handleClick}>4</button>
-                    <button className="calc-button" onClick={handleClick}>5</button>
-                    <button className="calc-button" onClick={handleClick}>6</button>
+                    <button className="calc-button" onClick={() => setInput(input+4)}>4</button>
+                    <button className="calc-button" onClick={() => setInput(input+5)}>5</button>
+                    <button className="calc-button" onClick={() => setInput(input+6)}>6</button>
                     <button className="calc-button calc-button-op">-</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button" onClick={handleClick}>1</button>
-                    <button className="calc-button" onClick={handleClick}>2</button>
-                    <button className="calc-button" onClick={handleClick}>3</button>
+                    <button className="calc-button" onClick={() => setInput(input+1)}>1</button>
+                    <button className="calc-button" onClick={() => setInput(input+2)}>2</button>
+                    <button className="calc-button" onClick={() => setInput(input+3)}>3</button>
                     <button className="calc-button calc-button-op">+</button>
                 </div>
                 <div className="calc-row">
-                    <button className="calc-button width-2" onClick={handleClick}>0</button>
+                    <button className="calc-button width-2" onClick={() => setInput(input+0)}>0</button>
                     <button className="calc-button">.</button>
                     <button className="calc-button calc-button-op">=</button>
                 </div>

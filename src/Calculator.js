fade8b06c97ef6c50c85e6580d6a6a88297a0692
General Assembly <generalassembly@Davids-MacBook-Pro.local>
2020-08-31 21:05:02 -0700

    [operation] Add operations, attempt adding second value

--- src/Calculator.js
+++ src/Calculator.js
@@ -2,29 +2,46 @@ import React, { useState } from 'react'
 
 const Calculator = props => {
     // Declare state variables
-    const [value, setValue] = useState('');
+    const [value1, setValue1] = useState('')
+    // const [value2, setValue2] = useState('')
+    const [op, setOp] = useState('')
+    // const [equals, setEquals] = useState(false)
 
     function displayNum(num) {
-        setValue(value + num)
+        // console.log('𝟏', value1, '𝟐', value2)
+        // if (!operation) {
+            setValue1(value1 + num)
+        // } else if (operation) {
+            // setValue2(value2 + num)
+        // }
+    }
+
+    function operation(symbol) {
+        setOp(symbol)
     }
 
     function clearVal(e) {
-        setValue('')
+        setValue1('')
+        setOp('')
+        // setValue2('')
     }
 
     return (
         <div className="container">
             <h1>React Calculator</h1>
             <div className="calc-container">
-                <p>Values: </p>
-                <div className="answer-box">{value}</div>
+                <p>{value1}{op}</p>
+                {/* {value2}{equals} */}
+                <div className="answer-box">{value1}</div>
                 <div className="calc-row">
                     <button className="calc-button calc-button-top"
                         onClick={(e) => {clearVal(e)}}
                     >AC</button>
                     <button className="calc-button calc-button-top">+/-</button>
                     <button className="calc-button calc-button-top">%</button>
-                    <button className="calc-button calc-button-op">/</button>
+                    <button className="calc-button calc-button-op"
+                        onClick={() => {operation(' / ')}}
+                    >/</button>
                 </div>
                 <div className="calc-row">
                     <button
@@ -39,7 +56,9 @@ const Calculator = props => {
                         className="calc-button"
                         onClick={() => {displayNum(9)}}
                     >9</button>
-                    <button className="calc-button calc-button-op">x</button>
+                    <button className="calc-button calc-button-op"
+                        onClick={() => {operation(' X ')}}
+                    >x</button>
                 </div>
                 <div className="calc-row">
                     <button
@@ -54,7 +73,9 @@ const Calculator = props => {
                         className="calc-button"
                         onClick={() => {displayNum(6)}}
                     >6</button>
-                    <button className="calc-button calc-button-op">-</button>
+                    <button className="calc-button calc-button-op"
+                        onClick={() => {operation(' - ')}}
+                    >-</button>
                 </div>
                 <div className="calc-row">
                     <button
@@ -69,14 +90,18 @@ const Calculator = props => {
                         className="calc-button"
                         onClick={() => {displayNum(3)}}
                     >3</button>
-                    <button className="calc-button calc-button-op">+</button>
+                    <button className="calc-button calc-button-op"
+                        onClick={() => {operation(' + ')}}
+                    >+</button>
                 </div>
                 <div className="calc-row">
                     <button
                         className="calc-button width-2"
                         onClick={() => {displayNum(0)}}
                     >0</button>
-                    <button className="calc-button">.</button>
+                    <button className="calc-button"
+                        onClick={() => {displayNum('.')}}
+                    >.</button>
                     <button className="calc-button calc-button-op">=</button>
                 </div>
             </div>

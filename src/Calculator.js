e79a8202a7d56f79da5dc489195cd51b32d16458
margare-jihua <margaret.jihua@gmail.com>
2020-09-01 00:00:22 -0700

    [Calculate] basic operator and equal

--- src/Calculator.js
+++ src/Calculator.js
@@ -4,10 +4,8 @@ import Input from './Input'
 const Calculator = props => {
     // Declare state variables
     var [input, setInput] = useState('')
-    var [currentNum, setCurrentNum] = useState('')
     var [previousNum, setPreviousNum] = useState('')
     var [operator, setOperator] = useState('')
-    var numbers = ['0','1','2','3','4','5','6','7','8','9']
 
     // DECIMAL . BUTTON
     function addDecimal() {
@@ -30,6 +28,70 @@ const Calculator = props => {
     // AC CLEAR BUTTON
     function clear() {
         setInput('')
+        setPreviousNum('')
+        setOperator('')
+    }
+
+    // ADD + BUTTON
+    function add() {
+        if (operator) {
+            alert('Sorry, only one operator allowed')
+        }
+        setPreviousNum(input)
+        setOperator('+')
+        setInput('')
+    }
+
+    // SUBTRACT - BUTTON
+    function subtract() {
+        if (operator) {
+            alert('Sorry, only one operator allowed')
+        }
+        setPreviousNum(input)
+        setOperator('-')
+        setInput('')
+    }
+
+    // MULTIPLY X BUTTON
+    function multiply() {
+        if (operator) {
+            alert('Sorry, only one operator allowed')
+        }
+        setPreviousNum(input)
+        setOperator('*')
+        setInput('')
+    }
+
+    // DIVIDE / BUTTON
+    function divide() {
+        if (operator) {
+            alert('Sorry, only one operator allowed')
+        }
+        setPreviousNum(input)
+        setOperator('/')
+        setInput('')
+    }
+
+    // EQUARE = BUTTON
+    function equare() {
+        if (operator === '+') {
+            setInput (
+                Number(previousNum)+Number(input)
+            )
+        } else if (operator === '-') {
+            setInput (
+                Number(previousNum)-Number(input)
+            )
+        } else if (operator === '*') {
+            setInput (
+                Number(previousNum)*Number(input)
+            )
+        } else if (operator === '/'){
+            setInput (
+                Number(previousNum)/Number(input)
+            )
+        }
+        // Reuse the result
         setOperator('')
     }
 
@@ -37,35 +99,35 @@ const Calculator = props => {
         <div className="container">
             <h1>React Calculator</h1>
             <div className="calc-container">
-                <Input input={input} />
+                <Input input={input} previousNum={previousNum} />
                 <div className="calc-row">
                     <button className="calc-button calc-button-top" onClick={clear}>AC</button>
-                    <button className="calc-button calc-button-top">+/-</button>
+                    <button className="calc-button calc-button-top">+/-</button> 
                     <button className="calc-button calc-button-top">%</button>
-                    <button className="calc-button calc-button-op">/</button>
+                    <button className="calc-button calc-button-op" onClick={divide}>/</button>
                 </div>
                 <div className="calc-row">
                     <button className="calc-button" onClick={() => setInput(input+7)}>7</button>
                     <button className="calc-button" onClick={() => setInput(input+8)}>8</button>
                     <button className="calc-button" onClick={() => setInput(input+9)}>9</button>
-                    <button className="calc-button calc-button-op">x</button>
+                    <button className="calc-button calc-button-op" onClick={multiply}>x</button>
                 </div>
                 <div className="calc-row">
                     <button className="calc-button" onClick={() => setInput(input+4)}>4</button>
                     <button className="calc-button" onClick={() => setInput(input+5)}>5</button>
                     <button className="calc-button" onClick={() => setInput(input+6)}>6</button>
-                    <button className="calc-button calc-button-op">-</button>
+                    <button className="calc-button calc-button-op" onClick={subtract}>-</button>
                 </div>
                 <div className="calc-row">
                     <button className="calc-button" onClick={() => setInput(input+1)}>1</button>
                     <button className="calc-button" onClick={() => setInput(input+2)}>2</button>
                     <button className="calc-button" onClick={() => setInput(input+3)}>3</button>
-                    <button className="calc-button calc-button-op">+</button>
+                    <button className="calc-button calc-button-op" onClick={add}>+</button>
                 </div>
                 <div className="calc-row">
                     <button className="calc-button width-2" onClick={addZero}>0</button>
                     <button className="calc-button" onClick={addDecimal}>.</button>
-                    <button className="calc-button calc-button-op">=</button>
+                    <button className="calc-button calc-button-op" onClick={equare}>=</button>
                 </div>
             </div>
         </div>
--- src/Input.js
+++ src/Input.js
@@ -1,6 +1,10 @@
 import React from 'react'
 
 export default function Input (props) {
-    let input = (props.input) ? props.input : 0
+    // let input = (props.input) ? props.input : 0
+    let input
+    if (props.input) {input=props.input}
+    else if (props.previousNum) {input=props.previousNum}
+    else {input=0}
     return <div className="answer-box">{input}</div>
 }
